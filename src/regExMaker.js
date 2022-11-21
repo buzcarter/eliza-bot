ef141f38d45cad884fbd2694b18ae3ec27f3fb0f
BuzCarter <bcarter@fandango.com>
2022-11-21 07:51:19 -0800

    [node] expand synonyms

--- src/regExMaker.js
+++ src/regExMaker.js
@@ -7,88 +7,89 @@ const RegExStr = {
 
 const regExes = {
   /** finds "`@happy`" within "`* i am* @happy *`" */
-  AT_VAR_NAMES: /@(\S+)/,
+  USE_SYNONYM: /@(\S+)/g,
+
   /** Inline "*", finds "`u *r`" within "`* do you *remember *`" */
   INLINE_WILDCARD: /(\S)\s*\*\s*(\S)/,
+
   /** Starts with a wildcard, finds "`* a`" within "`* are you *`" */
   STARTS_WITH_WILDCARD: /^\s*\*\s*(\S)/,
+
   /** Ends with a wildcard, finds "`u *`" within "`* are you *`" */
   ENDS_WITH_WILDCARD: /(\S)\s*\*\s*$/,
+
   /** looks for "`*`" even if it's surrounded by spaces "`   *   `" */
   WILDCARD: /^\s*\*\s*$/,
+
   /** One or more spaces (anywhere) */
   WHITESPACE: /\s+/g,
+
+  BEGINS_WITH_MEM_FLAG: /^\$\s*/,
 };
 
-const synPatterns = {};
+const synPatternHash = {};
 
 // generate synonym list
 function buildSynonymHash() {
   if (langConfig.synonyms && typeof langConfig.synonyms === 'object') {
     // eslint-disable-next-line guard-for-in, no-restricted-syntax
     for (const key in langConfig.synonyms) {
-      synPatterns[key] = `(${key}|${langConfig.synonyms[key].join('|')})`;
+      synPatternHash[key] = `(${key}|${langConfig.synonyms[key].join('|')})`;
     }
   }
 }
 
 function make(pattern) {
   let useMemFlag = false;
-  let regEx = pattern;
+  let regEx = `${pattern || ''}`;
 
-  // check mem flag and store it as decomp's element 2
-  if (regEx.charAt(0) === '$') {
-    let offset = 1;
-    while (regEx.charAt[offset] === ' ') offset++;
-    regEx = regEx.substring(offset);
+  if (regExes.BEGINS_WITH_MEM_FLAG.test(regEx)) {
+    regEx = regEx.replace(regExes.BEGINS_WITH_MEM_FLAG, '');
     useMemFlag = true;
   }
 
   // expand synonyms
-  let atVarsMatches = regExes.AT_VAR_NAMES.exec(regEx);
-  while (atVarsMatches) {
-    const synonTxt = synPatterns[atVarsMatches[1]] ? synPatterns[atVarsMatches[1]] : atVarsMatches[1];
-    regEx = regEx.substring(0, atVarsMatches.index) + synonTxt + regEx.substring(atVarsMatches.index + atVarsMatches[0].length);
-    atVarsMatches = regExes.AT_VAR_NAMES.exec(regEx);
+  if (regExes.USE_SYNONYM.test(regEx)) {
+    regEx = regEx.replace(regExes.USE_SYNONYM, (match, word) => synPatternHash[word] || word);
   }
 
-  // expand asterisk expressions
+  // expand `*` (wildcard) expressions
   if (regExes.WILDCARD.test(regEx)) {
     regEx = RegExStr.WILDCARD;
   } else {
-    let inlineMatches = regExes.INLINE_WILDCARD.exec(regEx);
-    if (inlineMatches) {
+    if (regExes.INLINE_WILDCARD.test(regEx)) {
+      let matches = regExes.INLINE_WILDCARD.exec(regEx);
       let leftPart = '';
       let rightPart = regEx;
-      while (inlineMatches) {
-        leftPart += rightPart.substring(0, inlineMatches.index + 1);
-        if (inlineMatches[1] !== ')') {
+      while (matches) {
+        leftPart += rightPart.substring(0, matches.index + 1);
+        if (matches[1] !== ')') {
           leftPart += RegExStr.BOUNDARY;
         }
         leftPart += RegExStr.WILDCARD;
-        if ((inlineMatches[2] !== '(') && (inlineMatches[2] !== '\\')) {
+        if ((matches[2] !== '(') && (matches[2] !== '\\')) {
           leftPart += RegExStr.BOUNDARY;
         }
-        leftPart += inlineMatches[2];
-        rightPart = rightPart.substring(inlineMatches.index + inlineMatches[0].length);
-        inlineMatches = regExes.INLINE_WILDCARD.exec(rightPart);
+        leftPart += matches[2];
+        rightPart = rightPart.substring(matches.index + matches[0].length);
+        matches = regExes.INLINE_WILDCARD.exec(rightPart);
       }
       regEx = leftPart + rightPart;
     }
 
-    const startsMatches = regExes.STARTS_WITH_WILDCARD.exec(regEx);
-    if (startsMatches) {
+    if (regExes.STARTS_WITH_WILDCARD.test(regEx)) {
+      const matches = regExes.STARTS_WITH_WILDCARD.exec(regEx);
       let patternTxt = RegExStr.WILDCARD;
-      if ((startsMatches[1] !== ')') && (startsMatches[1] !== '\\')) {
+      if ((matches[1] !== ')') && (matches[1] !== '\\')) {
         patternTxt += RegExStr.BOUNDARY;
       }
-      regEx = patternTxt + regEx.substring(startsMatches.index - 1 + startsMatches[0].length);
+      regEx = patternTxt + regEx.substring(matches.index - 1 + matches[0].length);
     }
 
-    const endsMatches = regExes.ENDS_WITH_WILDCARD.exec(regEx);
-    if (endsMatches) {
-      let patternTxt = regEx.substring(0, endsMatches.index + 1);
-      if (endsMatches[1] !== '(') {
+    if (regExes.ENDS_WITH_WILDCARD.test(regEx)) {
+      const matches = regExes.ENDS_WITH_WILDCARD.exec(regEx);
+      let patternTxt = regEx.substring(0, matches.index + 1);
+      if (matches[1] !== '(') {
         patternTxt += RegExStr.BOUNDARY;
       }
       regEx = `${patternTxt}${RegExStr.WILDCARD}`;
